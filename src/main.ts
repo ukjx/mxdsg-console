1ac2a7eb0b4d18b3d222fb66cce7e084c47929a6
1 <1>
2024-12-17 14:04:02 +0800

    feat: vercel部署报错处理2

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 // import './style.css'
 import './assets/index.css'
-import app from './app.vue'
+import App from './App.vue'
 import router from './router.ts'
 
-createApp(app).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
