a3d2ac5bfc826057516b25ce79b3ac228b38c9c1
1 <1>
2025-03-05 11:56:44 +0800

    feat:更新组件增加textarea

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import type { Updater } from '@tanstack/vue-table'
+import type { Ref } from 'vue'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
@@ -5,37 +7,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function getTimeDifference(time: string): string {
-  const startTime = new Date(time.replace(/-/g, '/'));
-  const diff = new Date().getTime() - startTime.getTime();
-
-  const hours = Math.floor(diff / (1000 * 60 * 60));
-  const minutes = Math.floor((diff / (1000 * 60)) % 60);
-  const seconds = Math.floor((diff / 1000) % 60);
-
-  let timeDiff = '';
-  if (hours > 0) {
-    timeDiff += `${hours}小时`;
-  }
-  if (minutes > 0) {
-    timeDiff += `${minutes}分`;
-  }
-  if (seconds >= 0) {
-    timeDiff += `${seconds}秒`;
-  }
-
-  return timeDiff.trim();
+export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref) {
+  ref.value
+    = typeof updaterOrValue === 'function'
+      ? updaterOrValue(ref.value)
+      : updaterOrValue
 }
-
-export function formatTime(time: string): string {
-  const date = new Date(time.replace(/-/g, '/').replace('T', ' ').substring(0, 19));
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '');
-  const day = String(date.getDate()).padStart(2, '');
-  const hours = String(date.getHours()).padStart(2, '');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
-
